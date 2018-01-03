befe6725b8c57bae9b29863bae3474acfe5fb392
danilradkovsky <dradkovsky@readdle.com>
2018-01-03 13:03:55 +0200

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+const gulp = require('gulp');
+const less = require('gulp-less');
+const path = require('path');
+const autoprefixer = require('gulp-autoprefixer');
+const clean_css = require('gulp-clean-css');
+
+ 
+gulp.task('less', function () {
+  return gulp.src('./styles/main.less')
+    .pipe(less({
+      paths: [ path.join(__dirname, 'less', 'includes') ]
+    }))
+    .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+    .pipe(clean_css({level: 1}))
+    .pipe(gulp.dest('./styles'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('./styles/*.less', ['less']);  // Watch all the .less files, then run the less task
+});
+
+gulp.task('default', ['watch']); // Default will run the 'entry' watch task
